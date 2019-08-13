22a01068bf505f66ffe508c1cd28c411c6933087
John Coene <jcoenep@gmail.com>
2019-08-13 12:50:58 +0200

    init

---
+++ R/setup.R
@@ -0,0 +1,20 @@
+#' Initialise Session
+#' 
+#' Initialise a session, installs TextAnalysis Juia dependency if needed.
+#' 
+#' @param ... Arguments passed to \link[JuliaCall]{julia_setup}.
+#' 
+#' @import JuliaCall
+#' @import assertthat
+#' 
+#' @name init
+#' @export
+init_textanalysis <- function (...){
+  julia <- JuliaCall::julia_setup(...)
+  JuliaCall::julia_install_package_if_needed("TextAnalysis")
+  JuliaCall::julia_library("TextAnalysis")
+}
+
+#' @name init
+#' @export
+setup_textanalysis <- init_textanalysis
