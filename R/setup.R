814c2c48201df89b9ad2d647932c8b9bd1371b48
JohnCoene <jcoenep@gmail.com>
2019-08-14 22:41:09 +0200

    init_stats might be removed

--- R/setup.R
+++ R/setup.R
@@ -5,10 +5,15 @@
 #' @param ... Arguments passed to \link[JuliaCall]{julia_setup}.
 #' 
 #' @section Packages:
-#' Packages installed are:
+#' Packages installed by \code{init_textanalysis} are:
 #' \itemize{
 #'   \item{\code{TextAnalysis}}
 #'   \item{\code{Languages}}
+#' }
+#' Packages installed by \code{init_stats} are:
+#' \itemize{
+#'   \item{\code{MultivariateStats}}
+#'   \item{\code{Clustering}}
 #' }
 #' 
 #' @import JuliaCall
@@ -16,13 +21,22 @@
 #' 
 #' @name init
 #' @export
-init_textanalysis <- function (...){
+init_textanalysis <- function(...){
   julia <- JuliaCall::julia_setup(...)
   JuliaCall::julia_install_package_if_needed("TextAnalysis")
   JuliaCall::julia_install_package_if_needed("Languages")
   JuliaCall::julia_library("TextAnalysis")
 }
 
-#' @name init
+#' @rdname init
+#' @export
+init_stats <- function(){
+  JuliaCall::julia_install_package_if_needed("MultivariateStats")
+  JuliaCall::julia_install_package_if_needed("Clustering")
+  JuliaCall::julia_library("MultivariateStats")
+  JuliaCall::julia_library("Clustering")
+}
+
+#' @rdname init
 #' @export
 setup_textanalysis <- init_textanalysis
