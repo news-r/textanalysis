c95447c8e1b26a11b10de6db9134e04974efe2bf
John Coene <jcoenep@gmail.com>
2019-08-16 14:57:42 +0200

    set Julia seed added

---
+++ R/helpers.R
@@ -0,0 +1,17 @@
+#' Set Seed in Julia
+#' 
+#' Set a seed in Julia, similar to \code{set.seed} in R.
+#' 
+#' @param x Seed, an integer.
+#' 
+#' @examples
+#' \dontrun{set_seed(42)}
+#' 
+#' @export
+set_seed <- function(x) {
+  assert_that(is_missing(x))
+  x <- as.integer(x)
+  julia_library("Random")
+  expr <- paste0("Random.seed!(", x,")")
+  julia_eval(expr)
+}
